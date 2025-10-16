2a639f694e39d01005a56fc034dbaf6bf8b68ee4
sier6519-tech <sier6519@laudioalde.eus>
2025-10-16 14:11:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('ctaButton').addEventListener('click', function() {
+    alert('¡Gracias por tu interés! Un mensaje con los detalles de inscripción y pago será enviado a la brevedad. ¡Nos vemos en MOTORTEC!');
+});
